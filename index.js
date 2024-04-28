10da947371963c0841bb4a1c65a5f547d057c67e
Biderohi <biderohimohapatra@gmail.com>
2024-04-28 20:07:53 +0530

    cp

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
 const DarkModeCheckbox = document.querySelector('#darkmode')
-DarkModecheckbox.addEventListener('change',(e) => {
-console.log('DarkModeCheckbox.cheked');
+// lightmode
+const contDarkModeCheckbox = document.querySelector('#lightmode')
+const cont = document.querySelector('cont')
+
+
+
+DarkModecheCkbox.addEventListener('change',(e) => {
+console.log('DarkModeCheckbox.checked');
+})
+contDarkModecheCkbox.addEventListener('change',(e) => {
+    console.log('contDarkModeCheckbox.checked');
+    })
+
+
+
+
+const fullDarkModeCheckbox = document.querySelector('#full-dark-mode')
+const containedDarkModeCheckbox = document.querySelector('#contained-dark-mode')
+const container = document.querySelector('.container')
+
+const isFullDarkMode = JSON.parse(localStorage.getItem('fullDarkModeCheckbox'))
+const isContainedDarkMode = JSON.parse(
+  localStorage.getItem('containedDarkModeCheckbox')
+)
+
+if (isFullDarkMode) {
+  fullDarkModeCheckbox.checked = true
+}
+if (isContainedDarkMode) {
+  containedDarkModeCheckbox.checked = true
+}
+
+changeFullDarkMode()
+changeContainedDarkMode()
+
+fullDarkModeCheckbox.addEventListener('change', () => {
+  changeFullDarkMode()
+  changeContainedDarkMode()
 })
 
+function changeFullDarkMode() {
+  changeTheme(fullDarkModeCheckbox, document.body, 'fullDarkModeCheckbox')
+}
 
+function changeContainedDarkMode() {
+  changeTheme(containedDarkModeCheckbox, container, 'containedDarkModeCheckbox')
+}
 
+function changeTheme(checkbox, element, localStorageKey) {
+  if (checkbox.checked) {
+    element.classList.add('dark')
+    localStorage.setItem(localStorageKey, true)
+  } else {
+    element.classList.remove('dark')
+    localStorage.setItem(localStorageKey, false)
+  }
+}
 
-// const fullDarkModeCheckbox = document.querySelector('#full-dark-mode')
-// const containedDarkModeCheckbox = document.querySelector('#contained-dark-mode')
-// const container = document.querySelector('.container')
-
-// const isFullDarkMode = JSON.parse(localStorage.getItem('fullDarkModeCheckbox'))
-// const isContainedDarkMode = JSON.parse(
-//   localStorage.getItem('containedDarkModeCheckbox')
-// )
-
-// if (isFullDarkMode) {
-//   fullDarkModeCheckbox.checked = true
-// }
-// if (isContainedDarkMode) {
-//   containedDarkModeCheckbox.checked = true
-// }
-
-// changeFullDarkMode()
-// changeContainedDarkMode()
-
-// fullDarkModeCheckbox.addEventListener('change', () => {
-//   changeFullDarkMode()
-//   changeContainedDarkMode()
-// })
-
-// function changeFullDarkMode() {
-//   changeTheme(fullDarkModeCheckbox, document.body, 'fullDarkModeCheckbox')
-// }
-
-// function changeContainedDarkMode() {
-//   changeTheme(containedDarkModeCheckbox, container, 'containedDarkModeCheckbox')
-// }
-
-// function changeTheme(checkbox, element, localStorageKey) {
-//   if (checkbox.checked) {
-//     element.classList.add('dark')
-//     localStorage.setItem(localStorageKey, true)
-//   } else {
-//     element.classList.remove('dark')
-//     localStorage.setItem(localStorageKey, false)
-//   }
-// }
-
-// containedDarkModeCheckbox.addEventListener('change', changeContainedDarkMode)
+containedDarkModeCheckbox.addEventListener('change', changeContainedDarkMode)
